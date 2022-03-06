76043e6ef08fc13f9f11837f5719342a877936b5
Yuan Cheng <79687050+YuanChengRua@users.noreply.github.com>
2022-03-05 22:51:55 -0500

    Update App.js

--- components/src/App.js
+++ components/src/App.js
@@ -2,12 +2,19 @@ import './App.css';
 import React from 'react';
 
 // atob is deprecated but this function converts base64string to text string
+// const decodeFileBase64 = (base64String) => {
+//   // From Bytestream to Percent-encoding to Original string
+//   return decodeURIComponent(
+//     atob(base64String).split("").map(function (c) {
+//       return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
+//     }).join("")
+//   );
+// };
+
 const decodeFileBase64 = (base64String) => {
   // From Bytestream to Percent-encoding to Original string
   return decodeURIComponent(
-    atob(base64String).split("").map(function (c) {
-      return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join("")
+    atob(base64String)
   );
 };
 
